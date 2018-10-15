10975a7e64a725d44bf26a6053d8382cf57b7a05
Alfredo Benassi <alfrebenassi@gmail.com>
2018-10-15 12:46:29 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tda-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ngx-text-diff-app';
+}
