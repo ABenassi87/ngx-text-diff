f38ae54691bec5f5967696ab4fd4ecc12fa1e6ec
Alfredo Benassi <alfrebenassi@gmail.com>
2018-10-28 16:45:47 -0300

    Create and publish npm package #3
      - Added npm config.
    
    Resolves #3

--- projects/ngx-text-diff/src/public_api.ts
+++ projects/ngx-text-diff/src/public_api.ts
@@ -4,4 +4,8 @@
 
 export * from './lib/ngx-text-diff.service';
 export * from './lib/ngx-text-diff.component';
+export * from './lib/loader-sppiner/loader-sppiner.component';
+export * from './lib/format-line.pipe';
+export * from './lib/ngx-text-diff.model';
+export * from './lib/ngx-text-diff.utils';
 export * from './lib/ngx-text-diff.module';
